b75a86762d7d44c94f83378952142c83d4a0d63b
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-10-09 22:44:43 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+
+import Home from './components/pages/Home'
+import Users from './components/pages/Users'
+import Contact from './components/pages/Contact'
+
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users/>} />
+        <Route path="/allusers" element={<Navigate to="/users"/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
+
+//npm install
+//npm install react-router-dom@latest
